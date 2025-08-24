de088793e8e960d525205327f41e16e94591c3ef
burmeai.io <athuyrain@gmail.com>
2025-08-25 03:30:16 +0630

    Update rollup-config.js

--- rollup-config.js
+++ rollup-config.js
@@ -4,7 +4,7 @@ import commonjs from "@rollup/plugin-commonjs";
 export default {
   input: "src/index.js",
   output: {
-    file: "dist/burme-npl.js",
+    file: "main/burme-npl.js",
     format: "umd",
     name: "BurmeNPL"
   },
